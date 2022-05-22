8233a57c20c71145c6cfc5260581d258b2dd4786
DiegoPC-UOC <101181055+DiegoPC-UOC@users.noreply.github.com>
2022-05-22 13:05:11 +0200

    Create SensoresCocina.py

---
+++ Producto2/SensoresCocina.py
@@ -0,0 +1,54 @@
+import paho.mqtt.client as paho
+import random
+import time
+import sys
+#Variables de conexion con la informacion por defecto
+broker="localhost"
+port=1883
+#Comprueba si el usuario ha intoducido dos parametros desde la consola al ejecutar el script
+if len(sys.argv) == 3:
+    #Si lo hace asigna el primer parametro como la ip del host de destino
+    broker = sys.argv[1]
+    #Y el segundo parametro como el puerto de destino
+    port =  int(sys.argv[2])
+#Funcion que genera un numero aleatorio en un rango dado para los lux
+def generarLux():
+    lux = random.randrange(50,10000)
+    mensaje = lux
+    return lux
+#Funcion que recoge un cliente y envia un mensaje a su nombre, con el valor de lux generado anteriormente
+def envioMensajeLux(cliente,topico):
+    lumen = generarLux()
+    #Concatenacion del mensaje
+    mensaje = str(lumen)
+    #Envio del mensaje
+    cliente.publish(topico, mensaje)
+    cliente.loop
+#Creacion de clientes para el envio de temperaturas asignandoles un nombre
+clienteLuz1 = paho.Client("CocinaLumen1")
+clienteLuz2 = paho.Client("CocinaLumen2")
+clienteLuz3 = paho.Client("CocinaLumen3")
+clienteLuz4 = paho.Client("CocinaLumen4")
+topico1 = "casa/luz/cocina/s1"
+topico2 = "casa/luz/cocina/s2"
+topico3 = "casa/luz/cocina/s3"
+topico4 = "casa/luz/cocina/s4"
+#Creacion de la conexion para los clientes
+clienteLuz1.connect(broker, port)
+clienteLuz2.connect(broker, port)
+clienteLuz3.connect(broker, port)
+clienteLuz4.connect(broker, port)
+#Bucle infinito para el envio de mensajes
+while True:
+    envioMensajeLux(clienteLuz1,topico1)
+    #Parar la ejecucion del script 2 segundos
+    time.sleep(2)
+    envioMensajeLux(clienteLuz2,topico2)
+    #Parar la ejecucion del script 2 segundos
+    time.sleep(2)
+    envioMensajeLux(clienteLuz3,topico3)
+    #Parar la ejecucion del script 2 segundos
+    time.sleep(2)
+    envioMensajeLux(clienteLuz4,topico4)
+    #Parar la ejecucion del script 2 segundos
+    time.sleep(2)
